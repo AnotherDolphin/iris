cea34b7b6f58b93b2e415fc91772d7f25575ccf9
Abdullah Nayer <abdullahnayer@gmail.com>
2023-04-24 03:08:52 +0200

    old dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-## Flask serving IRIS ML model predictions - using Docker
+## Dockerized Flask app serving IRIS model predictions
+
+This is a simple Flask app that serves predictions from a pre-trained model. The model is a simple logistic regression model trained on the IRIS dataset. The model is saved as a pickle file and is loaded into the app at runtime.
+
+The app is dockerized and can be run locally or deployed to a cloud provider.
+
+### Running the app locally
+
+1. Clone the repo
+2. Build the docker image: `docker build -t iris-app .`
+3. Run the docker container: `docker run -p 5000:5000 iris-app`
+4. Navigate to `http://localhost:5000/` to confirm the app is running
+4. Make a prediction request
+
+#### Example request:
+
+    - [POST] http://localhost:5000/predict
+    - Content-Type: application/json
+    - Body: {"sepal_length": 5.1, "sepal_width": 3.5, "petal_length": 1.4, "petal_width": 0.2}
+
+#### Example response:
+`{"prediction": ""Iris-setosa""}`
+
+### No time to run locally? Try it out Live!
+
+The app is deployed on Google Cloud Run and can be accessed at the following URL:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 import pickle
 import numpy as np
@@ -7,30 +8,32 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 # predict function
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 5)
+    to_predict = np.array(to_predict_list).reshape(1, 4)
     result = model.predict(to_predict)
     return result[0]
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Alexandre!'
 
-@app.route('/a')
-def hello_worsld():
-    return 'Hasdfd!'
-
-# predict route
 @app.route('/predict', methods=['POST'])
 def predict():
-    to_predict_list = request.get_json()['input']
-    to_predict_list = list(map(int, to_predict_list))
+    to_predict_list = request.get_json()
+    to_predict_list = [to_predict_list['sepal_length'], to_predict_list['sepal_width'],
+                       to_predict_list['petal_length'], to_predict_list['petal_width']]
+    to_predict_list = list(map(float, to_predict_list))
     result = ValuePredictor(to_predict_list)
-    return result
+    return {'prediction': result}
+
+# for cloud run
+# if __name__ == '__main__':
+#     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 # enable debug
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
